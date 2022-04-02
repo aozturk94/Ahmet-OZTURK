38af16b676a4e427f3566aa56b983d5a55720c62
aozturk94 <98884819+aozturk94@users.noreply.github.com>
2022-04-03 02:13:41 +0300

    CSS sayfa içeriği bitirilecek.

--- 03_Proje/script.js
+++ 03_Proje/script.js
@@ -1,10 +1,56 @@
-$(document).ready(function(){
-    $("#navbarRooter div").click(function(){
-        // $(document).scrollTop(600);
-        // console.log(this.id)
-        let deneme = $("#"+this.className)
-        $(document).scrollTop($("#"+this.className).height());
+$(document).ready(function () {
+    $("#navbarRooter #deneme").click(function () {
+        var idPosition = ($("#" + this.className)).position().top;
+        var textScroll = $('#' + this.className).scrollTop();
+        var totalScroll = textScroll + idPosition;
+        // $(document).scrollTop(totalScroll);
+        $('html, body').animate({
+            scrollTop: totalScroll
+        }, 500);
+        
+       $(this).classList.toggle('active');
+        
+    });
 
-        http://www.pinusart.com/yazilar-76-jquery-menu-ile-sayfa-kaydirma-scrolling-tek-sayfa-tasarim.html
+    $("#hedef").click(function(){
+        $("#hedef").fadeTo(750,0.1) 
+    })   
+    $("#hedef").mouseleave(function(){
+        $("#hedef").fadeTo(1000,0.9) 
     })
-})
+
+
+    let sliderIndex = 0;
+    let allSlides = $(".slide").toArray();
+    let allDots = $(".dot").toArray();
+
+    $("#prevSlide").click(function () {
+        if (sliderIndex == 0) {
+            sliderIndex = allSlides.length
+        }
+        showSlide(--sliderIndex);
+    })
+    $("#nextSlide").click(function () {
+        if (sliderIndex == allSlides.length - 1) {
+            sliderIndex = -1;
+        }
+        showSlide(++sliderIndex)
+    })
+    $(".dot").click(function () {
+        sliderIndex = $(this).index()
+        showSlide(sliderIndex);
+    })
+
+    function showSlide(n) {
+        for (let i = 0; i < allSlides.length; i++) {
+            allSlides[i].style.display = "none";
+            allDots[i].classList.remove("active");
+        }
+
+        $(".slide").eq(n).fadeTo(300, 1);
+        allDots[n].classList.add("active");
+
+    }
+    showSlide(sliderIndex);
+});
+
