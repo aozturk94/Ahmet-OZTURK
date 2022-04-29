8b9aeb93cffc2823287348d1287ac01e8d7232c2
aozturk94 <98884819+aozturk94@users.noreply.github.com>
2022-04-29 19:22:47 +0300

    update

---
+++ DatabaseImageProject/DatabaseImageProject/Models/Concrete/DAL/ProductDAL.cs
@@ -0,0 +1,13 @@
+﻿using DatabaseImageProject.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseImageProject.Models.Concrete.DAL
+{
+    public class ProductDAL : BaseRepository<Product>
+    {
+
+    }
+}
---
+++ DatabaseImageProject/DatabaseImageProject/Models/Concrete/ProductDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DatabaseImageProject.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseImageProject.Models.Concrete
+{
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-MTU4EKB\\SQLEXPRESS;Database=ProductDbAksam;User=sa;Pwd=123;");
+            }
+        }
+    }
+}
