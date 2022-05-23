5bde1329ba08a8d856cb1160e11e7aa99eafc81a
aozturk94 <98884819+aozturk94@users.noreply.github.com>
2022-05-23 04:03:30 +0300

    update

---
+++ TravelAgencyBookingApp/Bus-Ticket-Booking/Bus-Ticket-Booking.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Bus_Ticket_Booking.Business.Abstract;
+using Bus_Ticket_Booking.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bus_Ticket_Booking.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ITicketService _ticketService;
+        public AdminController(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+        public IActionResult AdminList()
+        {
+            return View(new RouteTicket()
+            {
+                Tickets = _ticketService.GetAll()
+            });
+        }
+
+        public IActionResult CancelTicket(int ticketId)
+        {
+            var ticket = _ticketService.GetById(ticketId);
+            if (ticket != null)
+            {
+                _ticketService.Delete(ticket);
+            }
+            return RedirectToAction("AdminList");
+        }
+    }
+}
